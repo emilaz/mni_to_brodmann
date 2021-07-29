7ea0313660db66d94332f64409a8378049a3623c
Emil <emil.azadian@student.hu-berlin.de>
2021-07-30 00:05:58 +0200

    up to date

---
+++ README.md
@@ -0,0 +1,32 @@
+### MNI_TO_BRODMANN
+
+Simple script to otain brain areas of different hierachies from
+ either normalized MNI coordinates or talairach coordinates. 
+ Works by first converting MNI to talairach coordinates and then using the talairach client 
+ (as found under talairach.org/client) to obtain brain areas.
+ 
+### Requirements
+
+* Talairach Client, downloadable from http://www.talairach.org/talairach.jar
+* \>= Java 1.4
+
+### Instructions
+
+You will need a tab delimited text/csv file with only the (x,y,z) coordinates per electrode.
+Each electrode should be indicated by a newline (see coordinates.txt for an example file).
+
+Then, run main.py with the following arguments
+
+* -coords: Path to file with coordinates
+* -tal_file: Path for saving intermediate talairach coordinates. Default is `tal_coords.txt`
+* -client: Path to talairach client
+* -tal: Optional parameter. Use if passed coordinates are already talairach.
+* -cube_range: Cube range around coordinates for which regions are outputted. 
+See www.talairach.org/manual for more info. Default value is `3`.
+
+## Example
+
+From within this repo, run
+
+    python main.py -coords coordinates.txt -client path_to_client -tal_file delete_me_later.txt
+    
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ https://brainmap.org/icbm2tal/
 import numpy as np
 import subprocess
 import argparse
+import os
 
 
 def mni2tal(elecs):
@@ -33,45 +34,65 @@ def mni2tal(elecs):
     return tala
 
 
-def tal2brain(file_path, client, range):
+def tal2brain(file_path, client, cube_range):
     """
     Function to obtain brain areas of different hierachies. Presupposes that the Talairach daemon is installed.
     Input: Input file path, client path
     """
-    lel = subprocess.run(['java', '-cp', client, 'org.talairach.ExcelToTD', '3:{},{}'.format(range, file_path)], check=True,
+    # lel = subprocess.run(['java', '-cp', client, 'org.talairach.ExcelToTD', '3:{},{}'.format(cube_range, file_path)], check=True,
+    lel = subprocess.run(['java', '-cp', client, 'org.talairach.ExcelToTD', '4,{}'.format(file_path)], check=True,
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
                          # capture_output=True, text=True)
     print(lel.stdout)
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-coords", help="Path to MNI coordinates txt file")
-    parser.add_argument("-temp", help="File path to write talairach coordinates to. Needed for client",
-                        default='coordinates.txt')
-    parser.add_argument("-client", help="Path to Talairach Client")
-    parser.add_argument("-tal", help="Use if coordinates entered are already Talairach", action='store_true')
-    parser.add_argument("-cube_range", help='Cube range in which to search for area.'
-                                            ' See http://www.talairach.org/manual.html', default=3)
-    args = parser.parse_args()
-
+def main(coord_file, tal_file, client_path, tal=False, cube_range=3):
+    """
+    Function to call from other modules. Combining steps to convert MNI to areas
+    :param coord_file: File to txt file with coordinates
+    :param tal_file: Filename to save talairach coordinates to
+    :param client_path: Path to talairach client
+    :param tal: Bool indicating whether coordinates are already talairach
+    :param cube_range: Range around point for which regions are outputted
+    :return:
+    """
     # read in our coords:
     elecs = []
-    with open(args.coords, 'r') as f:
+    with open(coord_file, 'r') as f:
         for line in f:
             elecs.append([float(s) for s in line.split('\t')])  # assuming tab delimited coordinate file
 
     # now, convert to talairach if needed
-    if args.tal is not None:
+    if tal:
         tal_elecs = np.asarray(elecs)
     else:
         tal_elecs = mni2tal(np.asarray(elecs))
 
     # save them to file
-    tal_elecs_file = args.temp
-    with open(tal_elecs_file, 'w') as  f:
-        np.savetxt(f, tal_elecs, delimiter='\t')
+    tal_file = os.path.splitext(coord_file)[0] + '_tal.txt'
+    with open(tal_file, 'w') as f:
+        np.savetxt(f, tal_elecs, delimiter='\t', fmt='%.2f')
 
     # call client to recieve area info stuff
-    tal2brain(tal_elecs_file, args.client, args.cube_range)
-    print('Output file can be found under {}.td'.format(args.coords))
+    tal2brain(tal_file, client_path, cube_range)
+    brod_file = os.path.splitext(coord_file)[0] + '_brod_areas.txt'
+    os.rename('./{}.td'.format(tal_file), brod_file)
+    print('Done. Output file can be found under {}'.format(brod_file))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-coords", help="Path to MNI coordinates txt file")
+    parser.add_argument("-tal_file", help="File path to write talairach coordinates to. Needed for client",
+                        default='tal_coords.txt')
+    parser.add_argument("-client", help="Path to Talairach Client")
+    parser.add_argument("-tal", help="Use if coordinates entered are already Talairach", action='store_true')
+    parser.add_argument("-cube_range", help='Cube range in which to search for area.'
+                                            ' See http://www.talairach.org/manual.html', default=3)
+    args = parser.parse_args()
+    if args.tal is not None:
+        tal=True
+    else:
+        tal=False
+    main(args.coords, args.tal_file, args.client, tal, args.cube_range)
+
+
